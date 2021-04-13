b22ab51a2907695cc258c49905601a17aa0a643e
ooooo <297872913@qq.com>
2021-04-13 21:49:46 +0800

    Redisson

--- spring-redis/README.md
+++ spring-redis/README.md
@@ -7,3 +7,7 @@
 
 
 ## redisson 操作
+
+1. 提供了**分布式锁**的实现
+2. JUC 包中各种分布式的实现，如 分布式map、分布式list
+3. 限流器：`org.redisson.RedissonRateLimiter`
---
+++ spring-redis/src/main/java/com/ooooo/config/RedissonConfig.java
@@ -0,0 +1,22 @@
+package com.ooooo.config;
+
+import com.ooooo.constant.ProfileConstants;
+import org.redisson.api.RedissonClient;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * @author leizhijie
+ * @since 2021/4/13 20:42
+ */
+@Configuration
+@Profile(ProfileConstants.PROFILE_REDISSON)
+public class RedissonConfig {
+	
+	@Bean
+	public RedissonConnectionFactory redissonConnectionFactory(RedissonClient redisson) {
+		return new RedissonConnectionFactory(redisson);
+	}
+}
