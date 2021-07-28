6954c9ec511a59bf0d6fc42ab70723b845092fc7
ooooo <297872913@qq.com>
2021-07-28 08:08:29 +0800

    grpc

---
+++ experimental-code/src/main/java/com/ooooo/grpc/GrpcServer.java
@@ -0,0 +1,48 @@
+package com.ooooo.grpc;
+
+import com.ooooo.grpc.helloworld.GreeterGrpc;
+import com.ooooo.grpc.helloworld.HelloReply;
+import com.ooooo.grpc.helloworld.HelloRequest;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import lombok.SneakyThrows;
+
+/**
+ * @author ooooo
+ * @date 2021/07/27 08:16
+ */
+public class GrpcServer {
+
+	public static final int PORT = 12345;
+
+	@SneakyThrows
+	public static void main(String[] args) {
+		Server server = ServerBuilder.forPort(PORT)
+						.addService(new GreeterImpl())
+						.build()
+						.start();
+		server.awaitTermination();
+	}
+
+	static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
+		@Override
+		public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+			System.out.println("receive message: " + request.getName());
+			HelloReply helloReply = HelloReply.newBuilder().setMessage(request.getName() + " say Hello").build();
+			responseObserver.onNext(helloReply);
+			responseObserver.onCompleted();
+		}
+
+		@Override
+		public void sayHi(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+			System.out.println("receive message: " + request.getName());
+			HelloReply helloReply = HelloReply.newBuilder().setMessage(request.getName() + " say Hi").build();
+			responseObserver.onNext(helloReply);
+			responseObserver.onCompleted();
+		}
+	}
+
+
+}
--- spring-bugs-report/src/test/java/com/ooooo/SpringMvcArgumentResolverApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.ooooo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class SpringMvcArgumentResolverApplicationTests {
-	
-}
