1695a7a25e2228aeeb1b3071dce9272fa86d5d18
ooooo <297872913@qq.com>
2021-09-14 20:18:58 +0800

    actuator
    Signed-off-by: ooooo <297872913@qq.com>

--- spring-boot-examples/spring-boot-redis/src/main/java/com/ooooo/RedisApplication.java
+++ spring-boot-examples/spring-boot-redis/src/main/java/com/ooooo/RedisApplication.java
@@ -2,6 +2,7 @@ package com.ooooo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
  */
 @SpringBootApplication
 @EnableRedisRepositories
+@EnableCaching
 public class RedisApplication {
 
 	public static void main(String[] args) {
