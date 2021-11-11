cdaa598be3e9c8c119c557aeab4508d9eb3aa8a9
ooooo <297872913@qq.com>
2021-11-11 14:48:41 +0800

    add module[spring-boot-test] Junit5Test

--- spring-boot-examples/spring-boot-test/src/test/java/com/ooooo/service/Junit5Test.java
+++ spring-boot-examples/spring-boot-test/src/test/java/com/ooooo/service/Junit5Test.java
@@ -1,7 +1,18 @@
 package com.ooooo.service;
 
 import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,31 +20,45 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.condition.JRE.JAVA_11;
+import static org.junit.jupiter.api.condition.JRE.JAVA_8;
+import static org.junit.jupiter.api.condition.OS.LINUX;
+import static org.junit.jupiter.api.condition.OS.MAC;
 
 /**
  * @author <a href="https://github.com/ooooo-youwillsee">ooooo</a>
  * @date 2021/11/10 16:54
  * @since 1.0.0
  */
+@Slf4j
 @SpringBootTest
 public class Junit5Test {
 	
 	@Autowired
 	private UserService userService;
 	
+	@BeforeAll
+	static void initAll() {
+		log.info("initAll");
+	}
+	
+	@BeforeEach
+	void init() {
+		log.info("init");
+	}
+	
+	
 	@Test
-	public void simpleTest() {
+	void simpleTest() {
 		String username = userService.findUsernameById(1L);
-		
 		assertEquals("username: 1", username);
 	}
 	
 	
 	@ParameterizedTest
 	@MethodSource("p1")
-	public void methodSourceTest(Long id) {
+	void methodSourceTest(Long id) {
 		String username = userService.findUsernameById(id);
-		
 		assertThat(username.contains(id.toString()));
 	}
 	
@@ -51,5 +76,48 @@ public class Junit5Test {
 	
 	static Stream<Object[]> p2() {
 		return Stream.of(new Object[]{1L, "tom"}, new Object[]{2L, "jerry"});
+	}
+	
+	
+	@RepeatedTest(10)
+	void repeatedTest() {
+		String username = userService.findUsernameById(1L);
+		assertEquals("username: 1", username);
+	}
+	
+	
+	@Test
+	@Disabled("Disabled until bug xxx has been resolved")
+	void testWillBeSkipped() {
+	}
+	
+	@Test
+	@EnabledOnOs({LINUX, MAC})
+	void onLinuxOrMac() {
+	}
+	
+	@Test
+	@EnabledForJreRange(min = JAVA_8, max = JAVA_11)
+	void fromJava8to11() {
+	}
+	
+	@Test
+	@EnabledIfSystemProperty(named = "os.arch", matches = ".*64.*")
+	void onlyOn64BitArchitectures() {
+	}
+	
+	@Test
+	@EnabledIfEnvironmentVariable(named = "ENV", matches = "staging-server")
+	void onlyOnStagingServer() {
+	}
+	
+	@AfterEach
+	void tearDown() {
+		log.info("tearDown");
+	}
+	
+	@AfterAll
+	static void tearDownAll() {
+		log.info("tearDownAll");
 	}
 }
