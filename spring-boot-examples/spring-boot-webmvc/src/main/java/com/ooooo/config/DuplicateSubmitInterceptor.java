9a97a2c0b7de9095bd18a7dccea6ba8ebd3a0494
ooooo <297872913@qq.com>
2021-11-22 16:35:11 +0800

    add module[spring-boot-webmvc] DuplicateSubmitInterceptor

---
+++ spring-boot-examples/spring-boot-webmvc/src/main/java/com/ooooo/config/DuplicateSubmitInterceptor.java
@@ -0,0 +1,82 @@
+package com.ooooo.config;
+
+import java.util.concurrent.TimeUnit;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import static java.lang.String.valueOf;
+
+/**
+ * 三秒内防重复提交
+ *
+ * @author <a href="https://github.com/ooooo-youwillsee">ooooo</a>
+ * @date 2021/11/22 16:10
+ * @since 1.0.0
+ */
+@Slf4j
+@Component
+public class DuplicateSubmitInterceptor implements HandlerInterceptor {
+	
+	/**
+	 * must configure redisTemplate
+	 */
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
+	
+	private static final String REDIS_LOCK_PREFIX = "idempotency.submit.config:%s:%s:%s";
+	
+	/**
+	 * default lock time
+	 */
+	private static final int DEFAULT_LOCK_TIME = 3;
+	
+	/**
+	 * whether determine the request is same as the before
+	 */
+	public static final String PARAM_CHECK_REQUEST_MD5 = "CHECK_REQUEST_MD5";
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		if (handler instanceof HandlerMethod) {
+			HandlerMethod method = (HandlerMethod) handler;
+			if (method.getBeanType().isAnnotationPresent(Controller.class) || method.getBeanType().isAnnotationPresent(RestController.class)) {
+				
+				String requestURI = requestURI(request);
+				String md5 = request.getParameter(PARAM_CHECK_REQUEST_MD5);
+				// retrieve userId  from session or Authorization
+				String userId = retrieveUserId(request);
+				// build redis key
+				String redisKey = buildKey(userId, requestURI, md5);
+				
+				boolean exist = redisTemplate.hasKey(redisKey);
+				if (exist) {
+					throw new RuntimeException("请勿重新提交");
+				}
+				redisTemplate.boundValueOps(redisKey).set(valueOf(System.currentTimeMillis()), DEFAULT_LOCK_TIME, TimeUnit.SECONDS);
+			}
+		}
+		return true;
+	}
+	
+	private String retrieveUserId(HttpServletRequest request) {
+		return "0";
+	}
+	
+	public String buildKey(String userId, String url, String md5) {
+		return String.format(REDIS_LOCK_PREFIX, userId, url, md5);
+	}
+	
+	
+	private String requestURI(HttpServletRequest request) {
+		return request.getRequestURI();
+	}
+	
+}
