709a003e80f7404db58e7376efbe452fe689aad6
ooooo <297872913@qq.com>
2022-09-12 10:18:32 +0800

    RequestParamsPropertySource
    Signed-off-by: ooooo <297872913@qq.com>

--- spring-boot-examples/spring-boot-webmvc/src/main/java/com/ooooo/autoconfigure/env/dev/RequestParamsPropertySource.java
+++ spring-boot-examples/spring-boot-webmvc/src/main/java/com/ooooo/autoconfigure/env/dev/RequestParamsPropertySource.java
@@ -1,11 +1,12 @@
 package com.ooooo.autoconfigure.env.dev;
 
 import com.ooooo.autoconfigure.env.AbstractSimplePropertySource;
-import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static com.ooooo.constant.EnvironmentConstants.ENV_PREFIX;
 import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
@@ -26,26 +27,19 @@ public class RequestParamsPropertySource extends AbstractSimplePropertySource {
 		String propertyKey = ENV_PREFIX + name.replace(".", "_");
 		String propertyValue = null;
 		try {
-			// 先请求参数中获取
-			ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
-			if (attr == null) {
-				return null;
-			}
-			HttpServletRequest request = attr.getRequest();
-			if (request == null) {
-				return null;
-			}
-			propertyValue = request.getParameter(propertyKey);
-			if (StringUtils.isBlank(propertyValue)) {
-				// 再从请求头中获取
-				propertyValue = request.getHeader(propertyKey);
-			}
-		} catch (Throwable ignored) {
+      // 先请求参数中获取
+      ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+      HttpServletRequest request = attr.getRequest();
+      propertyValue = request.getParameter(propertyKey);
+      if (StringUtils.isBlank(propertyValue)) {
+        // 再从请求头中获取
+        propertyValue = request.getHeader(propertyKey);
+      }
+    } catch (Throwable ignored) {
 		}
 		// 空字符也当做null
 		propertyValue = defaultIfBlank(propertyValue, null);
 		return propertyValue;
-		
 	}
 	
 }
