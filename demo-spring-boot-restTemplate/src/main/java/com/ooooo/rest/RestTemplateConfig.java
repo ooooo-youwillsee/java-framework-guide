89576f2fb26ffbe6ef0e2af340dd31255c88263e
ooooo <297872913@qq.com>
2023-02-04 11:43:26 +0800

    fix: RestTemplateConfig
    Signed-off-by: ooooo <297872913@qq.com>

--- demo-spring-boot-restTemplate/src/main/java/com/ooooo/rest/RestTemplateConfig.java
+++ demo-spring-boot-restTemplate/src/main/java/com/ooooo/rest/RestTemplateConfig.java
@@ -1,12 +1,16 @@
 package com.ooooo.rest;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -21,12 +25,20 @@ public class RestTemplateConfig {
 	public RestTemplate restTemplate() {
 		ClientHttpRequestFactory clientHttpRequestFactory = new OkHttp3ClientHttpRequestFactory();
 		RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
-
+		
 		restTemplate.getInterceptors().add((request, body, execution) -> {
 			log.info(new String(body, StandardCharsets.UTF_8));
 			return execution.execute(request, body);
 		});
-		restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+		// message converter setting
+		for (HttpMessageConverter<?> messageConverter : restTemplate.getMessageConverters()) {
+			if (messageConverter instanceof StringHttpMessageConverter) {
+				((StringHttpMessageConverter) messageConverter).setDefaultCharset(StandardCharsets.UTF_8);
+			} else if (messageConverter instanceof MappingJackson2HttpMessageConverter) {
+				ObjectMapper objectMapper = ((MappingJackson2HttpMessageConverter) messageConverter).getObjectMapper();
+				objectMapper.setSerializationInclusion(Include.NON_NULL);
+			}
+		}
 		return restTemplate;
 	}
 }
