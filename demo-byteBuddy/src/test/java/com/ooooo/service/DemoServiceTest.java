87e4f8a1ec82dd68f45c35de9c816f8d69ff18fd
ooooo <297872913@qq.com>
2023-04-12 18:06:47 +0800

    demo-byteBuddy github issue DemoServiceTest

---
+++ demo-byteBuddy/src/test/java/com/ooooo/service/DemoServiceTest.java
@@ -0,0 +1,63 @@
+package com.cairh.cpe.counter.generic;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.implementation.bind.annotation.AllArguments;
+import net.bytebuddy.implementation.bind.annotation.Origin;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
+import net.bytebuddy.implementation.bind.annotation.SuperCall;
+import org.junit.jupiter.api.Test;
+
+import static net.bytebuddy.matcher.ElementMatchers.isDeclaredBy;
+import static net.bytebuddy.matcher.ElementMatchers.not;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * <a href="https://github.com/raphw/byte-buddy/issues/1428">github issue</a>
+ *
+ * @author <a href="https://github.com/ooooo-youwillsee">ooooo</a>
+ * @since 1.0.0
+ */
+public class DemoServiceTest {
+	
+	@Test
+	public void test() {
+		Class<?> clazz = new ByteBuddy()
+				.subclass(A.class)
+				.method(not(isDeclaredBy(Object.class)))
+				.intercept(MethodDelegation.to(new AInterceptor()))
+				.implement(B.class)
+				.intercept(FixedValue.value(new A()))
+				.make()
+				.load(Thread.currentThread().getContextClassLoader())
+				.getLoaded();
+		
+		assertTrue(B.class.isAssignableFrom(clazz));
+	}
+	
+	public static class A {
+		
+		public String sayHello() {
+			return "Hello";
+		}
+	}
+	
+	public interface B {
+		
+		A getA();
+	}
+	
+	public static class AInterceptor {
+		
+		@RuntimeType
+		public Object invoke(@RuntimeType @AllArguments Object[] args,
+		                     @Origin Method method,
+		                     @SuperCall Callable<Object> callable) throws Exception {
+			
+			return callable.call();
+		}
+	}
+}
